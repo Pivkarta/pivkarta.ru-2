a68e17787f9a930fabbb3a42bae40e64c9af4072
Dima <linklib.web@yandex.ru>
2021-05-18 07:26:34 +0300

    Change city url

--- src/components/ui/Link/City/index.tsx
+++ src/components/ui/Link/City/index.tsx
@@ -4,16 +4,20 @@ import Link from '..'
 import { CityLinkProps } from './interfaces'
 
 /**
- * Ссылка на страницу тега
+ * Ссылка на страницу города
  */
 const CityLink: React.FC<CityLinkProps> = ({ city, children, ...other }) => {
   return useMemo(() => {
     return (
-      <Link href={`/${city.alias}`} title={city.name} {...other}>
+      <Link
+        href={`/${city.alias}/@${city.lat},${city.lng},12`}
+        title={city.name}
+        {...other}
+      >
         {children || city.name}
       </Link>
     )
-  }, [city.alias, city.name, other, children])
+  }, [city.alias, city.name, city.lat, city.lng, other, children])
 }
 
 export default CityLink
